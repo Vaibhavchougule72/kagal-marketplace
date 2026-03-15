488439f403a51f6046010dede2ad9469228fb82c
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-03-15 19:16:27 +0530

    updated templates for github image storage

--- marketplace/github_storage.py
+++ marketplace/github_storage.py
@@ -8,6 +8,10 @@ def upload_image_to_github(file, folder):
     username = os.environ.get("GITHUB_USERNAME")
     repo = os.environ.get("GITHUB_REPO")
 
+    if not token or not username or not repo:
+        print("❌ GitHub environment variables missing")
+        return None
+
     file_content = base64.b64encode(file.read()).decode("utf-8")
 
     file_name = file.name
@@ -15,20 +19,24 @@ def upload_image_to_github(file, folder):
     url = f"https://api.github.com/repos/{username}/{repo}/contents/{folder}/{file_name}"
 
     headers = {
-        "Authorization": f"token {token}"
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json"
     }
 
     data = {
-        "message": f"upload {file_name}",
+        "message": f"Upload {file_name}",
         "content": file_content
     }
 
     response = requests.put(url, json=data, headers=headers)
 
-    if response.status_code == 201:
+    if response.status_code in [200, 201]:
 
         cdn_url = f"https://cdn.jsdelivr.net/gh/{username}/{repo}/{folder}/{file_name}"
 
         return cdn_url
+
+    else:
+        print("❌ GitHub Upload Failed:", response.text)
 
     return None
