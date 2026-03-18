a34551a7de0ddc20ad9623a79e40d20d6b8d5349
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-03-18 13:16:45 +0530

    sms last

--- marketplace/sms_service.py
+++ marketplace/sms_service.py
@@ -6,23 +6,22 @@ def send_sms(phone, message):
     url = "https://www.fast2sms.com/dev/bulkV2"
 
     payload = {
-        "sender_id": "FSTSMS",
         "message": message,
         "language": "english",
-        "route": "q",  # transactional
+        "route": "v",
         "numbers": phone
     }
 
     headers = {
-        "authorization": "nkfLSNdqHIXW5U6uJteboBVGpFsxOc8ryZTE320KjwPl7aA4v1JAXPqmpIHC9e0axfTBn5oyGOEkdFRN",
+        "authorization": os.getenv("FAST2SMS_API_KEY"),
         "Content-Type": "application/json"
     }
 
     try:
         response = requests.post(url, json=payload, headers=headers)
+        print("SMS RESPONSE:", response.json())   # 🔥 IMPORTANT DEBUG
         return response.json()
 
     except Exception as e:
         print("SMS Error:", e)
         return None
-
