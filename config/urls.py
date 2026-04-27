078cdeb0531f8b4a879a1f83c3d3f2771042e110
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-04-27 18:13:25 +0530

    assetlink

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.templatetags.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,13 +10,16 @@ urlpatterns = [
     path(
         '.well-known/assetlinks.json',
         RedirectView.as_view(
-            url=static('.well-known/assetlinks.json'),
+            url='/static/.well-known/assetlinks.json',
             permanent=False
-        )
+        ),
     ),
 
     path('', include('marketplace.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
