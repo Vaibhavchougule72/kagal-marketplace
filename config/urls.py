3a8ad451f47994e1febff4a158df4c8d3d071f99
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-04-27 18:22:21 +0530

    asset links

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,31 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.contrib import admin
+
+def assetlinks(request):
+    return JsonResponse([
+        {
+            "relation": [
+                "delegate_permission/common.handle_all_urls"
+            ],
+            "target": {
+                "namespace": "android_app",
+                "package_name": "com.example.lokamarketplace",
+                "sha256_cert_fingerprints": [
+                    "CB:04:E9:84:4E:27:42:50:AC:D3:D2:D9:D5:DA:9F:F9:8A:12:A5:6E:37:2B:C3:8D:C0:7B:30:DA:83:2E:65:EE"
+                ]
+            }
+        }
+    ], safe=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path(
         '.well-known/assetlinks.json',
-        RedirectView.as_view(
-            url='/static/.well-known/assetlinks.json',
-            permanent=False
-        ),
+        assetlinks
     ),
 
     path('', include('marketplace.urls')),
