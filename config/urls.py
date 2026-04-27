91463199cffd3e3028a6009c145f7f4116742dc8
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-04-27 18:01:41 +0530

    smooth chrome

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,20 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        '.well-known/assetlinks.json',
+        RedirectView.as_view(
+            url=static('.well-known/assetlinks.json'),
+            permanent=False
+        )
+    ),
+
     path('', include('marketplace.urls')),
 ]
 
