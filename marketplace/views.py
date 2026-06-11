61808f41e32441e162988de2b722dc1ae1088232
Vaibhav Chougule <chougulevaibhav72@gmail.com>
2026-06-11 08:32:22 +0530

    without nav bar

--- marketplace/views.py
+++ marketplace/views.py
@@ -1035,6 +1035,7 @@ def checkout(request):
         "store": store,
         "items": items,
         "show_floating_cart": False,
+        "show_navbar": False,
         "simple_navbar": False,
     }
 
